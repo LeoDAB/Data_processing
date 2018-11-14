cefe5114d8a130be1caaaa7460e4a36635df8a1c
LeoDAB <31652036+LeoDAB@users.noreply.github.com>
2018-11-14 16:43:36 +0100

    Update Homework/Week 2/EDA.py

--- Homework/Week 2/EDA.py
+++ Homework/Week 2/EDA.py
@@ -4,8 +4,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-pd.set_option("display.max_columns", 10)
-pd.set_option("display.max_rows", 1000)
 
 INPUT_CSV = "input.csv"
 OUTPUT_JSON = "EDA.json"
@@ -17,9 +15,8 @@ def clean_df(infile):
     """
     Note that calculating statistics with pandas always excludes NaN values. Therefore, missing values and unknowns
     are replaced with an NaN instead of deleting the complete row. The countries are still represented in the dataframe.
-
-    :param infile:
-    :return:
+    :param infile: input csv
+    :return: cleaned dataframe
     """
     # create dataframe from csv with only the useful columns
     df = pd.read_csv(infile, usecols=COLUMNS)
@@ -38,37 +35,94 @@ def clean_df(infile):
     return df
 
 def central_tendancy(column):
+    """
+    Within this function the central tendancy is calculated of a dataframe column. Some additional information is
+    given in Visual_Analysis.txt
+    :param column: dataframe column for calculations
+    :return: mean, meadian, mode and standard deviation of the column
+    """
 
-    meanCol = round(column.mean(), 2)
-    medianCol = column.median()
-    modeCol = column.mode()
-    stdCol = round(column.std(), 2)
+    # determine the outlier, based on initial histogram plots an outlier was spotted: see Visual_Analysis.txt
+    outlier = df[df['GDP ($ per capita) dollars'] > df['GDP ($ per capita) dollars'].mean()
+                  + 3 * df['GDP ($ per capita) dollars'].std()]
+    outGDP = outlier['GDP ($ per capita) dollars'][193]
 
-    # Histogram of the GDP per capita
-    hist = column.hist(bins=20)
-    hist.plot()
-    plt.title('GDP from countries worldwide', fontsize=12)
-    plt.ylabel('frequency')
-    plt.xlabel('GDP: dollars($) per capita')
+    # calculate the multiple measures of indicating the central tendancy
+    meanCol = round(column[column < outGDP].mean(), 2)
+    medianCol = column[column < outGDP].median()
+    modeCol = column[column < outGDP].mode()
+    stdCol = round(column[column < outGDP].std(), 2)
+
+    # Histograms of the GDP per capita, the second subplot excludes the outlier Suriname
+    fig, axs = plt.subplots(1, 2)
+    column.hist(ax=axs[0], bins=20, rwidth=0.9)
+    column.hist(ax=axs[1], bins=20, range=(column.min(), column[column < outGDP].max()), rwidth=0.9)
+    fig.suptitle('GDP from countries worldwide', fontsize=14)
+    fig.text(0.5, 0.04, 'GDP: dollars($) per capita', ha='center')
+    fig.text(0.04, 0.5, 'frequency', va='center', rotation='vertical')
     plt.show()
 
-    return meanCol, modeCol, medianCol, stdCol
+    return meanCol, medianCol, modeCol, stdCol
+
+def five_num_sum(column):
+    """
+
+    :param column:
+    :return:
+    """
+
+    # calculate the descriptive statistics: the five number summary
+    minimum = column.min()
+    FQ = column.quantile(q=0.25)
+    medianCol = column.median()
+    TQ = column.quantile(q=0.75)
+    maximum = column.max()
+
+    # create a boxplot
+    boxplot = df.boxplot(column='Infant mortality (per 1000 births)')
+    boxplot.plot()
+    plt.title('Infant mortality of countries world wide', fontsize=12)
+    plt.ylabel('Count')
+    plt.show()
+
+
+    return minimum, FQ, medianCol, TQ, maximum
 
 def JSON(df):
+    """
 
-    # Convert all data into a JSON file, taking country as index
-    df.set_index("Country", inplace=True)
-    df.to_json(OUTPUT_JSON, orient="index")
+    :param df:
+    :return:
+    """
+    # convert the dataframe to a JSON, country is the index
+    df.set_index('Country', inplace=True)
+    df.to_json(OUTPUT_JSON, orient='index')
+
 
 if __name__ == "__main__":
-    # add the necessary data to the global dictionary
+    # put the iput csv in a dataframe and clean for usage
     df = clean_df(INPUT_CSV)
-    meanGDP, medianGDP, modeGDP, stdGDP = central_tendancy(df['GDP ($ per capita) dollars'])
-    JSON(df)
 
+    # calculate the central tendancy of the GDP column and create a histogram
+    meanGDP, medianGDP, modeGDP, stdGDP = central_tendancy(df['GDP ($ per capita) dollars'])
     print(f"Mean of GDP: {meanGDP}")
     print(f"Median of GDP: {medianGDP}")
     print(f"Mode of GDP: {modeGDP}")
     print(f"Standard Deviation of GDP: {stdGDP}")
+
+    # calculate the 5 numbers summary and create a boxplot of the infant mortality column
+    infantMin, infantFQ, infantMed, infantTQ, infantMax = five_num_sum(df['Infant mortality (per 1000 births)'])
+    # Print 5-number summary
+    print('Min: %.3f' % infantMin)
+    print('Q1: %.3f' % infantFQ)
+    print('Median: %.3f' % infantMed)
+    print('Q3: %.3f' % infantTQ)
+    print('Max: %.3f' % infantMax)
+
+    # create a JSON file for the data
+    JSON(df)
+
+
+
 
 
