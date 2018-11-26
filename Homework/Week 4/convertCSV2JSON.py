24f79907978655628dbb03aa85ae05b3045bfe01
LeoDAB <31652036+LeoDAB@users.noreply.github.com>
2018-11-26 11:00:27 +0100

    start

---
+++ Homework/Week 4/convertCSV2JSON.py
@@ -0,0 +1,15 @@
+# Name: Leonie Bouma
+# Student number: 10898050
+
+import pandas as pd
+
+INFILE = 'data.csv'
+OUTPUT = 'data.json'
+# Ik wil een JSON voor station de Bilt met datum als key en RH als value
+df = pd.read_csv(INFILE, sep=',', skiprows=12, header=None, names=['station', 'date', 'rain (0.1 mm)']) # , skipinitialspace=True geeft floats
+
+# Convert the dataframe to a JSON, country is the index
+df.set_index('date', inplace=True)
+df.to_json(OUTPUT, orient='index')
+
+
