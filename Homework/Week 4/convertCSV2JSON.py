a45f481ca9a7c8575f20bb143087df2de502808b
LeoDAB <31652036+LeoDAB@users.noreply.github.com>
2018-11-26 22:36:10 +0100

    axis en interactivity moeten nog

--- Homework/Week 4/convertCSV2JSON.py
+++ Homework/Week 4/convertCSV2JSON.py
@@ -9,13 +9,13 @@ OUTPUT = 'data.json'
 COLUMNS = ['LOCATION', 'MEASURE', 'TIME', 'Value']
 
 # Create dataframe from input csv
-data = pd.read_csv(INFILE, sep=',', usecols=COLUMNS)
+df = pd.read_csv(INFILE, sep=',', usecols=COLUMNS)
 
 # Replace empty strings and 0 with NaN
-data[COLUMNS] = data[COLUMNS].replace({'':np.NaN, '0':np.NaN})
+df[COLUMNS] = df[COLUMNS].replace({'':np.NaN, '0':np.NaN})
 
 # Create a dataframe with the mean values for KTOE and PC_PRYENRGSUPPLY for each location
-df = data.groupby(['LOCATION', 'MEASURE'])['Value'].mean().unstack(level=-1)
+df = df.groupby(['LOCATION', 'MEASURE'])['Value'].mean().unstack(level=-1)
 
 # Convert the dataframe to a JSON
 df.to_json(OUTPUT, orient='index')
