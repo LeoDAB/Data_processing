1da0eaca2279d0b0eaae1d5b03b285a7a98e2d0c
LeoDAB <31652036+LeoDAB@users.noreply.github.com>
2018-11-26 12:33:51 +0100

    convert data to hopefully usable json

--- Homework/Week 4/convertCSV2JSON.py
+++ Homework/Week 4/convertCSV2JSON.py
@@ -2,14 +2,22 @@
 # Student number: 10898050
 
 import pandas as pd
+import numpy as np
 
 INFILE = 'data.csv'
 OUTPUT = 'data.json'
-# Ik wil een JSON voor station de Bilt met datum als key en RH als value
-df = pd.read_csv(INFILE, sep=',', skiprows=12, header=None, names=['station', 'date', 'rain (0.1 mm)']) # , skipinitialspace=True geeft floats
+COLUMNS = ['LOCATION', 'MEASURE', 'TIME', 'Value']
 
-# Convert the dataframe to a JSON, country is the index
-df.set_index('date', inplace=True)
+# Create dataframe from input csv
+data = pd.read_csv(INFILE, sep=',', usecols=COLUMNS)
+
+# Replace empty strings and 0 with NaN
+data[COLUMNS] = data[COLUMNS].replace({'':np.NaN, '0':np.NaN})
+
+# Create a dataframe with the mean values for KTOE and PC_PRYENRGSUPPLY for each location
+df = data.groupby(['LOCATION', 'MEASURE'])['Value'].mean().unstack(level=-1)
+
+# Convert the dataframe to a JSON
 df.to_json(OUTPUT, orient='index')
 
 
